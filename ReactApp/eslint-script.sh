b4cf4ee448f928328777c6c33ca13c79031403ba
JairoLedezma <61709408+JairoLedezma@users.noreply.github.com>
2021-08-24 20:26:14 -0500

    Update eslint-script.sh

--- ReactApp/eslint-script.sh
+++ ReactApp/eslint-script.sh
@@ -2,6 +2,6 @@
 
 cd /{JENKINS HOME DIRECTORY}/workspace/node-lint-pipeline
 
-eslint --no-color --format json --ext .ejs,.js --output-file /ReactApp/reports/eslint-report ./
+eslint --no-color --format json --ext .ejs,.js --output-file ./ReactApp/reports/eslint-report ./
 
 echo $? > /dev/null
